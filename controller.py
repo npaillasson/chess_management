45af619c35c88b5f7251db2bd81d2d944768d3c1
npaillasson <nathanpaillasson3@gmail.com>
2021-06-06 16:49:56 +0200

    update main menu feature and comments update

--- controller.py
+++ controller.py
@@ -4,19 +4,27 @@
 import os
 import model
 from view import Menu
+import time
+from datetime import datetime
 import re
 
+#permet de vérifier que le nom ne contient que des lettres
 NAME_CONTROL_EXPRESSION = re.compile(r"^[A-Za-z- ]+$")
 
-DATE_OF_BIRTH_EXPRESSION = re.compile(r"^[0-9]{2}/[0-9]{2}/[0-9]{4}")
+#permet de vérifier le format de la date
+DATE_OF_BIRTH_EXPRESSION = re.compile(r"^[0-9]{2}/[0-9]{2}/[0-9]{4}$")
 
+#permet de vérifier le format du rang du joueur
+RANK_EXPRESSION = re.compile(r"^[0-9]+$")
+
+#contient les choix du menu principale
 MAIN_MENU_CHOICES = ["Créer un joueur", "Créer un tournoi", "Gérer un tournoi en cours",
                      "Générer des rapports", "Règlages", "Quitter le logiciel"]
 
-CONFIRMATION_OR_CORRECTION_MENU = ["Valider !", "Corriger"]
+#contient les choix du menu de confirmation
+CORRECTION_MENU_CHOICES = ["Valider !", "Corriger", "Annuler"]
 
-PLAYERS_INFORMATION_CORRECTION_MENU = ["Nom", "Prénom", "Date de naissance", "Genre", "Rang"]
-
+#NE SEMBLE SERVIR A RIEN
 TOURNAMENTS_INFORMATION_CORRECTION_MENU = ["Nom", "Lieu", "Date", "Nombre de tours", "Joueurs",
                                            "Contrôle du temps", "Déscription"]
 
@@ -24,50 +32,63 @@ TOURNAMENTS_INFORMATION_CORRECTION_MENU = ["Nom", "Lieu", "Date", "Nombre de tou
 class Browse:
     """class which manage the navigation of the user according to his input"""
 
+    #Initi
     def __init__(self, main_menu_choices=MAIN_MENU_CHOICES,
-                 confirmation_or_correction_menu=CONFIRMATION_OR_CORRECTION_MENU,
-                 players_information_correction_menu=PLAYERS_INFORMATION_CORRECTION_MENU,
+                 correction_menu_choices=CORRECTION_MENU_CHOICES,
                  tournaments_information_correction_menu=TOURNAMENTS_INFORMATION_CORRECTION_MENU):
 
         self.main_menu_choices = main_menu_choices
-        self.confirmation_or_correction_menu = confirmation_or_correction_menu
-        self.players_information_correction_menu = players_information_correction_menu
+        self.correction_menu_choices = correction_menu_choices
         self.tournaments_information_correction_menu = tournaments_information_correction_menu
 
-        self.menu = Menu(self.main_menu_choices)
+        self.menu = Menu(self.main_menu_choices, self.correction_menu_choices)
 
 
-    def main_menu_ctrl(self):
+    def main_menu_control(self):
         """main menu controller function"""
         choice = self.menu.main_menu()
         if choice == 0: # Lancer la fonction de creation de joueur
-            self.player_creator_ctrl()
+            self.player_creator_control()
         elif choice == 1: # lancer la fonction de création de tournoi
             pass
     #A continuer!
 
-    def player_creator_ctrl(self):
+    #fonction principale du menu de création de joueur
+    def player_creator_control(self):
         """function which control the user input"""
         player_information = {}
-        last_name = self.player_last_name_creator_ctrl()
+        last_name = self.player_last_name_creator_control()
         if last_name:
             player_information["last_name"] = last_name
-            first_name = self.player_first_name_creator_ctrl()
+            first_name = self.player_first_name_creator_control()
             if first_name:
                 player_information["first_name"] = first_name
-                date_of_birth = self.player_date_of_brith_creator_ctrl()
+                date_of_birth = self.player_date_of_brith_creator_control()
                 if date_of_birth:
                     player_information["date_of_birth"] = date_of_birth
-                    print(player_information)
+                    rank = self.player_rank_creator_control()
+                    if rank:
+                        player_information["rank"] = rank
+                        choice = self.menu.player_validation(player_information,
+                                                             self.correction_menu_choices)
+                        if choice == 0:
+                            print("OK!")
+                            self.main_menu_control()
+                        if choice == 1:
+                            self.player_creator_control()
+                        else:
+                            self.main_menu_control()
+                    else:
+                        self.main_menu_control()
                 else:
-                    self.main_menu_ctrl()
+                    self.main_menu_control()
             else:
-                self.main_menu_ctrl()
+                self.main_menu_control()
         else:
-            self.main_menu_ctrl()
+            self.main_menu_control()
 
-
-    def player_last_name_creator_ctrl(self):
+    #fonction secondaire du menu de création des joueur récupère le nom
+    def player_last_name_creator_control(self):
         """player last name creator menu controller function"""
 
         while True:
@@ -79,7 +100,8 @@ class Browse:
             else:
                 print("Nom incorect")
 
-    def player_first_name_creator_ctrl(self):
+    # fonction secondaire du menu de création des joueur récupère le prénom
+    def player_first_name_creator_control(self):
         """player last name creator menu controller function"""
 
         while True:
@@ -91,7 +113,8 @@ class Browse:
             else:
                 print("Prénom incorect")
 
-    def player_date_of_brith_creator_ctrl(self):
+    # fonction secondaire du menu de création des joueur qui vérifie la validité de la date
+    def player_date_of_brith_creator_control(self):
         """player last name creator menu controller function"""
 
         while True:
@@ -99,14 +122,48 @@ class Browse:
             if date_of_birth == "quit":
                 return None
             elif DATE_OF_BIRTH_EXPRESSION.match(date_of_birth) is not None:
-                return date_of_birth
+
+                check_date_validity = self.date_checking(date_of_birth)
+                if check_date_validity:
+                    return date_of_birth
+                else:
+                    print("La date est incorect")
             else:
                 print("La date est incorect")
+
+    # fonction secondaire du menu de création des joueur récupère la date de naissance
+    def date_checking(self,date_of_birth):
+        """function which check the date validity"""
+        day_of_birth = int(date_of_birth[:2])
+        month_of_birth = int(date_of_birth[3:5])
+        year_of_birth = int(date_of_birth[6:])
+        try:
+            time_stamp = datetime(year_of_birth, month_of_birth, day_of_birth).timestamp()
+        except ValueError:
+            return False
+        else:
+            if time_stamp <= time.time():
+                return True
+            else:
+                return False
+
+    # fonction secondaire du menu de création des joueur récupère le rang du joueur
+    def player_rank_creator_control(self):
+        """player last name creator menu controller function"""
+
+        while True:
+            rank = self.menu.player_rank()
+            if rank == "quit":
+                return None
+            elif RANK_EXPRESSION.match(rank) is not None:
+                return rank
+            else:
+                print("Le rang rensseigné est incorect")
 
 
 
 
 
 browser = Browse()
-browser.main_menu_ctrl()
+browser.main_menu_control()
 
--- view.py
+++ view.py
@@ -7,9 +7,10 @@ from simple_term_menu import TerminalMenu
 class Menu:
     """Main menu class"""
 
-    def __init__(self, choices_main_menu_list):
-        """MainMenu Constructor"""
+    def __init__(self, choices_main_menu_list, choices_correction_menu):
+        """Menu Constructor"""
         self.choices_main_menu_list = choices_main_menu_list
+        self.choices_correction_menu = choices_correction_menu
 
     def main_menu(self):
         """Function which display the main menu"""
@@ -31,10 +32,26 @@ class Menu:
         return first_name
 
     def player_date_of_birth(self):
-        """Function which display the player first name menu"""
+        """Function which display the player date_of_birth menu"""
 
         date_of_birth = input("veuillez saisir la date de naissance du joueur: ")
         return date_of_birth
+
+    def player_rank(self):
+        """Function which display the player rank menu"""
+        player_rank = input("veuillez saisir le rang du joueur: ")
+        return player_rank
+
+    def player_validation(self, player_information, correction_menu_choices):
+        """function which allows to check and correct the player information"""
+        print("Nom : {} \nPrénom : {} \nDate de naissance : {}\nRang : {}".format(player_information["last_name"],
+                                                                                  player_information["first_name"],
+                                                                                  player_information["date_of_birth"],
+                                                                                  player_information["rank"]))
+        correction_menu = TerminalMenu(self.choices_correction_menu)
+        choice_index = correction_menu.show()
+        print(self.choices_correction_menu[choice_index])
+        return choice_index
 
 
 
