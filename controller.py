e54ed575f02f3f55b0edb89cdcfeb241d815afd0
npaillasson <nathanpaillasson3@gmail.com>
2021-06-06 14:30:18 +0200

    update controller.py and view.py

--- controller.py
+++ controller.py
@@ -4,6 +4,11 @@
 import os
 import model
 from view import Menu
+import re
+
+NAME_CONTROL_EXPRESSION = re.compile(r"^[A-Za-z- ]+$")
+
+DATE_OF_BIRTH_EXPRESSION = re.compile(r"^[0-9]{2}/[0-9]{2}/[0-9]{4}")
 
 MAIN_MENU_CHOICES = ["Créer un joueur", "Créer un tournoi", "Gérer un tournoi en cours",
                      "Générer des rapports", "Règlages", "Quitter le logiciel"]
@@ -31,14 +36,74 @@ class Browse:
 
         self.menu = Menu(self.main_menu_choices)
 
+
     def main_menu_ctrl(self):
         """main menu controller function"""
         choice = self.menu.main_menu()
         if choice == 0: # Lancer la fonction de creation de joueur
-            pass
+            self.player_creator_ctrl()
         elif choice == 1: # lancer la fonction de création de tournoi
             pass
     #A continuer!
+
+    def player_creator_ctrl(self):
+        """function which control the user input"""
+        player_information = {}
+        last_name = self.player_last_name_creator_ctrl()
+        if last_name:
+            player_information["last_name"] = last_name
+            first_name = self.player_first_name_creator_ctrl()
+            if first_name:
+                player_information["first_name"] = first_name
+                date_of_birth = self.player_date_of_brith_creator_ctrl()
+                if date_of_birth:
+                    player_information["date_of_birth"] = date_of_birth
+                    print(player_information)
+                else:
+                    self.main_menu_ctrl()
+            else:
+                self.main_menu_ctrl()
+        else:
+            self.main_menu_ctrl()
+
+
+    def player_last_name_creator_ctrl(self):
+        """player last name creator menu controller function"""
+
+        while True:
+            last_name = self.menu.player_last_name()
+            if last_name == "quit":
+                return None
+            elif NAME_CONTROL_EXPRESSION.match(last_name) is not None:
+                return last_name
+            else:
+                print("Nom incorect")
+
+    def player_first_name_creator_ctrl(self):
+        """player last name creator menu controller function"""
+
+        while True:
+            first_name = self.menu.player_first_name()
+            if first_name == "quit":
+                return None
+            elif NAME_CONTROL_EXPRESSION.match(first_name) is not None:
+                return first_name
+            else:
+                print("Prénom incorect")
+
+    def player_date_of_brith_creator_ctrl(self):
+        """player last name creator menu controller function"""
+
+        while True:
+            date_of_birth = self.menu.player_date_of_birth()
+            if date_of_birth == "quit":
+                return None
+            elif DATE_OF_BIRTH_EXPRESSION.match(date_of_birth) is not None:
+                return date_of_birth
+            else:
+                print("La date est incorect")
+
+
 
 
 
--- view.py
+++ view.py
@@ -17,3 +17,24 @@ class Menu:
         choice_index = main_menu.show()
         print(self.choices_main_menu_list[choice_index])
         return choice_index
+
+    def player_last_name(self):
+        """Function which display the player last name menu"""
+
+        last_name = input("veuillez saisir le nom du joueur: ")
+        return last_name
+
+    def player_first_name(self):
+        """Function which display the player first name menu"""
+
+        first_name = input("veuillez saisir le prénom du joueur: ")
+        return first_name
+
+    def player_date_of_birth(self):
+        """Function which display the player first name menu"""
+
+        date_of_birth = input("veuillez saisir la date de naissance du joueur: ")
+        return date_of_birth
+
+
+
